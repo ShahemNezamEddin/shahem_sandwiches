9df844003a16616cc3a2193d0ff8010e537fc932
ShahemNezamEddin <shahem.nezam.eddin@gmail.com>
2022-11-07 17:49:39 +0000

    Fix pylint warnings and add .pylintrc file

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ SCOPE_CREDS = CREDS.with_scopes(SCOPE)
 GSPEAD_CLIENT = gspread.authorize(SCOPE_CREDS)
 SHEET = GSPEAD_CLIENT.open("Love_sandwiches")
 
+
 def get_sales_data():
     """
     Get sales figures input from the user.
@@ -25,12 +26,12 @@ def get_sales_data():
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Inter your data here: ")
-
-        sales_data = data_str.split(",")
-        if validate_data(sales_data):
+        new_data = data_str.split(",")
+        if validate_data(new_data):
             print("data is valid")
             break
-    return sales_data
+    return new_data
+
 
 def validate_data(values):
     """
@@ -39,22 +40,26 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
-        [int(value) for value in values]
+        values = [int(value) for value in values]
         if len(values) != 6:
-            raise ValueError(f"Exactly 6 values required, you provided {len(values)}")
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
     return True
 
-def update_sales_worksheet(data):
+
+def update_sales_worksheet(sales):
     """
     Update sales worksheet, add new row with the list data provided
     """
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
+    sales_worksheet.append_row(sales)
     print("Sales worksheet updated successfully.\n")
+
 
 data = get_sales_data()
 sales_data = [int(num) for num in data]
